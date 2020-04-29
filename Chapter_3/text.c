e38a01a8878c44d045b4a15f6a86e0e3ea6498e7
AlainDlcTVOP <alain_dlc@outlook.com>
2020-04-29 20:17:19 +0200

    20200429

---
+++ Chapter_3/text.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/* read a line of text from terminal */
+int main()
+{
+   char line[81],character;
+   int c;
+   c=0;
+   printf("Enter text, Press <return> at end\n");
+   do
+   {
+      character=getchar();
+      line[c]=character;
+      c++;
+   } while (character!= '\n');
+   c=c-1;
+   line[c]= '\0';
+   printf("\n%s\n",line);
+
+   
+    return 0;
+}
